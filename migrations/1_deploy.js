ebdb1f39f433d39f61527bcbe0d905eb1a8d9afe
wojakpepechad <tokenalert7@gmail.com>
2024-01-05 05:26:12 +0100

    eth deploy

--- migrations/1_deploy.js
+++ migrations/1_deploy.js
@@ -2,7 +2,7 @@ const UniswapV2Factory = artifacts.require("UniswapV2Factory");
 
 module.exports = function(deployer) {
   // deployment steps
-  const feeSetter = '0xDF27046391D2f4F0Dbb8A4Ee995BDc49d22e5ee1'
+  const feeSetter = '0x2d98e72D82060549fA8464d6959d9919A2613230'
   deployer.deploy(UniswapV2Factory, feeSetter);
   
 };
